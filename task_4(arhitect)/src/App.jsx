b11703ef54ee1df24d3b2c73a12747b863bfe777
kheirkheberli <ali.xov08@gmail.com>
2025-10-22 11:20:00 +0400

    new folders

---
+++ task_4(arhitect)/src/App.jsx
@@ -0,0 +1,21 @@
+import Navbar from "./components/Navbar";
+import Pagest from "./components/Pagestart";
+import Projects from "./components/Projects";
+import About from "./components/About";
+import Contact from "./components/Contact";
+
+function App() {
+    return (
+        <div>
+
+            <Navbar />
+            <Pagest />
+            <Projects />
+            <About />
+            <Contact />
+
+        </div>
+    )
+}
+
+export default App;
