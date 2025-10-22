3b82a6bdb407e6b4ec972d210e9b70e55736842b
kheirkheberli <ali.xov08@gmail.com>
2025-10-22 11:41:09 +0400

    update

--- task_4(arhitect)/src/components/Projects.jsx
+++ task_4(arhitect)/src/components/Projects.jsx
@@ -1,4 +1,15 @@
 function Projects() {
+    const projectList = [
+        { id: 1, title: "Summer", image: "https://www.w3schools.com/w3images/house5.jpg" },
+        { id: 2, title: "Brick House", image: "https://www.w3schools.com/w3images/house2.jpg" },
+        { id: 3, title: "Renovated", image: "https://www.w3schools.com/w3images/house3.jpg" },
+        { id: 4, title: "Barn House", image: "https://www.w3schools.com/w3images/house4.jpg" },
+        { id: 5, title: "Summer", image: "https://www.w3schools.com/w3images/house2.jpg" },
+        { id: 6, title: "Brick House", image: "https://www.w3schools.com/w3images/house5.jpg" },
+        { id: 7, title: "Renovated", image: "https://www.w3schools.com/w3images/house4.jpg" },
+        { id: 8, title: "Barn House", image: "https://www.w3schools.com/w3images/house3.jpg" },
+    ];
+
     return (
         <>
             <div id="projects" className="w3-container w3-padding-32">
@@ -6,63 +17,25 @@ function Projects() {
                     Projects
                 </h2>
             </div>
+
             <div className="w3-row-padding">
-                <div className="w3-col l3 m6 w3-margin-bottom">
-                    <div className="w3-display-container">
-                        <div className="w3-display-topleft w3-black w3-padding">Summer</div>
-                        <img src="https://www.w3schools.com/w3images/house5.jpg" alt="House" style={{ width: "100%" }} />
-                    </div>
-                </div>
-                <div className="w3-col l3 m6 w3-margin-bottom">
-                    <div className="w3-display-container">
-                        <div className="w3-display-topleft w3-black w3-padding">
-                            Brick House
+                {projectList.map((project) => (
+                    <div className="w3-col l3 m6 w3-margin-bottom" key={project.id}>
+                        <div className="w3-display-container">
+                            <div className="w3-display-topleft w3-black w3-padding">
+                                {project.title}
+                            </div>
+                            <img
+                                src={project.image}
+                                alt={project.title}
+                                style={{ width: "100%" }}
+                            />
                         </div>
-                        <img src="https://www.w3schools.com/w3images/house2.jpg" alt="House" style={{ width: "100%" }} />
                     </div>
-                </div>
-                <div className="w3-col l3 m6 w3-margin-bottom">
-                    <div className="w3-display-container">
-                        <div className="w3-display-topleft w3-black w3-padding">Renovated</div>
-                        <img src="https://www.w3schools.com/w3images/house3.jpg" alt="House" style={{ width: "100%" }} />
-                    </div>
-                </div>
-                <div className="w3-col l3 m6 w3-margin-bottom">
-                    <div className="w3-display-container">
-                        <div className="w3-display-topleft w3-black w3-padding">Barn House</div>
-                        <img src="https://www.w3schools.com/w3images/house4.jpg" alt="House" style={{ width: "100%" }} />
-                    </div>
-                </div>
-                <div className="w3-col l3 m6 w3-margin-bottom">
-                    <div className="w3-display-container">
-                        <div className="w3-display-topleft w3-black w3-padding">Summer</div>
-                        <img src="https://www.w3schools.com/w3images/house2.jpg" alt="House" style={{ width: "99%" }} />
-                    </div>
-                </div>
-                <div className="w3-col l3 m6 w3-margin-bottom">
-                    <div className="w3-display-container">
-                        <div className="w3-display-topleft w3-black w3-padding">
-                            Brick House
-                        </div>
-                        <img src="https://www.w3schools.com/w3images/house5.jpg" alt="House" style={{ width: "99%" }} />
-                    </div>
-                </div>
-                <div className="w3-col l3 m6 w3-margin-bottom">
-                    <div className="w3-display-container">
-                        <div className="w3-display-topleft w3-black w3-padding">Renovated</div>
-                        <img src="https://www.w3schools.com/w3images/house4.jpg" style={{ width: "99%" }} />
-                    </div>
-                </div>
-                <div className="w3-col l3 m6 w3-margin-bottom">
-                    <div className="w3-display-container">
-                        <div className="w3-display-topleft w3-black w3-padding">Barn House</div>
-                        <img src="https://www.w3schools.com/w3images/house3.jpg" alt="House" style={{ width: "99%" }} />
-                    </div>
-                </div>
+                ))}
             </div>
         </>
-
-    )
+    );
 }
 
 export default Projects;
