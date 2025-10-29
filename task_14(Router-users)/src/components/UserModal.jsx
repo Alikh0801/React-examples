46834edf1a2781bf86337232abfb31c28e4fc624
kheirkheberli <ali.xov08@gmail.com>
2025-10-29 10:35:45 +0400

    fix

--- task_14(Router-users)/src/components/UserModal.jsx
+++ task_14(Router-users)/src/components/UserModal.jsx
@@ -6,7 +6,7 @@ function UserModal({ user, onClose }) {
                     onClick={onClose}
                     className="absolute right-3 top-2 text-gray-500 text-lg hover:text-red-500"
                 >
-                    ✕
+                    close
                 </button>
 
                 <img
